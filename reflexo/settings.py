b00719b70530c99c639cee43b4f3e36c0ba3d4cd
ZaithFSG <zaithfabrizio@gmail.com>
2025-09-09 12:21:54 -0500

    db change2

--- reflexo/settings.py
+++ reflexo/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'reflexo_db',
         'USER': 'root',
-        'PASSWORD': 'rodrigoxd147',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
